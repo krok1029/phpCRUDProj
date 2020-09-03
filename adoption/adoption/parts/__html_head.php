4a9a3af84ca64c52bb7d44c717a813c56f51406d
krok1029 <krok1029@gmail.com>
2020-09-03 14:01:49 +0800

    change fontawesome's url

--- adoption/adoption/parts/__html_head.php
+++ adoption/adoption/parts/__html_head.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= WEB_ROOT ?>/fontawesome/css/all.css">
+    <link rel="stylesheet" href="<?= WEB_ROOT ?>/../../fontawesome/css/all.css">
     <title><?= $page_title ?? '' ?>寵物領養</title>
 </head>
 
