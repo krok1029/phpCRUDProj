2753a2e0674645819ba83c428b7d83d378d3c04a
krok1029 <krok1029@gmail.com>
2020-09-07 15:12:27 +0800

    search ver 1.0

--- adoption/adoption/data-list.php
+++ adoption/adoption/data-list.php
@@ -8,7 +8,20 @@ $perPage = 5; // 每頁有幾筆資料
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
 $t_sql = "SELECT COUNT(1) FROM `pet_info_master`";
-$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+
+if (isset($_GET['like'])) {
+    $like = $_GET['like'];
+    $t_sql = "SELECT count(1) 
+    FROM `pet_info_master` a left join pet_info_detail b on a.pet_id = b.pet_id left join tag_list c on b.tag_id = c.tag_id 
+    WHERE a.name like '%$like%' or a.area like'%$like%' or a.address like '%$like%' or a.description like '%$like%' or c.description like '%$like%' or  a.dog_cat like '%$like%'
+    GROUP by a.`pet_id`, a.`name`, a.`dog_cat`, a.`age`, a.`area`, a.`address`, a.`description`, a.`create_time`";
+    $totalRows = $pdo->query($t_sql)->rowCount();
+    // echo $totalRows;
+} else {
+
+    $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+}
+
 // die('~~~'); //exit; // 結束程式
 $totalPages = ceil($totalRows / $perPage);
 
@@ -24,6 +37,16 @@ if ($totalRows > 0) {
     };
 
     $sql = sprintf("SELECT * FROM `pet_info_master` ORDER BY pet_id DESC LIMIT %s, %s", ($page - 1) * $perPage, $perPage);
+    $like = '';
+    if (isset($_GET['like'])) {
+        $like = $_GET['like'];
+        $tsql = "SELECT a.`pet_id`, a.`name`, a.`dog_cat`, a.`age`, a.`area`, a.`address`, a.`description`, a.`create_time` 
+        FROM `pet_info_master` a left join pet_info_detail b on a.pet_id = b.pet_id left join tag_list c on b.tag_id = c.tag_id 
+        WHERE a.name like '%$like%' or a.area like'%$like%' or a.address like '%$like%' or a.description like '%$like%' or c.description like '%$like%' or  a.dog_cat like '%$like%'
+        GROUP by a.`pet_id`, a.`name`, a.`dog_cat`, a.`age`, a.`area`, a.`address`, a.`description`, a.`create_time`";
+        $sql = sprintf("ORDER BY a.pet_id DESC LIMIT %s, %s", ($page - 1) * $perPage, $perPage);
+        $sql = $tsql . $sql;
+    }
     $stmt = $pdo->query($sql);
     $rows = $stmt->fetchAll();
 }
@@ -34,6 +57,24 @@ if ($totalRows > 0) {
 <?php include __DIR__ . '/parts/__navbar.php'; ?>
 <div class="container">
     <div class="row">
+        <div class="col d-flex justify-content-start">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item ">
+                        <input type="type" class="form-control" id="searchInput" name="searchInput" value="">
+                    </li>
+
+                    <li class="page-item ">
+                        <a class="page-link" href="javascript: like()">
+                            <i class="fas fa-search"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+
+
         <div class="col d-flex justify-content-end">
             <nav aria-label="Page navigation example">
                 <ul class="pagination">
@@ -47,11 +88,11 @@ if ($totalRows > 0) {
                         if ($i > $totalPages) break;
                     ?>
                         <li class="page-item <?= $i == $page ? 'active' : '' ?>">
-                            <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                            <a class="page-link" href="?like=<?= $like ?>&page=<?= $i ?>"><?= $i ?></a>
                         </li>
                     <?php endfor; ?>
                     <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>">
-                        <a class="page-link" href="?page=<?= $page + 1 ?>">
+                        <a class="page-link" href="?like=<?= $like ?>&page=<?= $page + 1 ?>">
                             <i class="fas fa-arrow-circle-right"></i>
                         </a>
                     </li>
@@ -82,7 +123,7 @@ if ($totalRows > 0) {
                 <?php if (!isset($_SESSION['admin1'])) : ?>
                     <th scope="col"><i class="fas fa-edit"></i></th>
                 <?php endif; ?>
-                <th scope="col"><i class="fas fa-heart"></i></th>
+                <!-- <th scope="col"><i class="fas fa-heart"></i></th> -->
 
 
             </tr>
@@ -121,7 +162,7 @@ if ($totalRows > 0) {
                     <?php if (!isset($_SESSION['admin1'])) : ?>
                         <td><a href="data-edit.php?pet_id=<?= $r['pet_id'] ?>"><i class="fas fa-edit"></i></a></td>
                     <?php endif; ?>
-                    <td><a href="#"><i class="fas fa-heart"></i></i></a></td>
+                    <!-- <td><a href="#"><i class="fas fa-heart"></i></i></a></td> -->
 
                 <?php endforeach; ?>
         </tbody>
@@ -148,6 +189,13 @@ if ($totalRows > 0) {
     $(document).ready(function() {
         $('[data-toggle="popover"]').popover();
     });
+</script>
+<script>
+    const like = () => {
+        const search = document.querySelector('#searchInput')
+        location.href = 'data-list.php?like=' + search.value
+
+    }
 </script>
 
 <?php include __DIR__ . '/parts/__html_foot.php'; ?>
